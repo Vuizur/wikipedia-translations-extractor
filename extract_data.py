ce414797b619c035aa1e1e31a98a928b80690c65
Hannes Krumbiegel <Vuizur@users.noreply.github.com>
2024-05-30 15:02:18 +0000

    Add error handling due to presumed stupid mediawiki bug

--- extract_data.py
+++ extract_data.py
@@ -31,22 +31,33 @@ FROM
 JOIN 
     langlinks ON page.page_id = langlinks.ll_from
 GROUP BY 
-    page.page_id
-LIMIT 10;
+    page.page_id;
 """)
+errored_words: list[str] = []
+
 # Write results to jsonl
-with open('output.jsonl', 'w') as file:
+with open('output.jsonl', 'w', encoding="utf-8") as file:
     for page_id, page_title, page_namespace, page_is_redirect, languages in tqdm(cursor):
         page_title = decode_bytes(page_title)
-        languages = json.loads(languages)
-        # Write the JSON line
-        file.write(json.dumps({
-            "page_id": page_id,
-            "title": page_title.replace('_', ' '),
-            "is_redirect": page_is_redirect,
-            "namespace": page_namespace,
-            "languages": languages
-        }, ensure_ascii=False) + '\n')
+        # I think because of this it can error: 
+        # https://stackoverflow.com/questions/44900545/wikipedia-database-dump-utf8-charset#comment76803654_44900545
+        try:
+            languages = json.loads(languages)
+            # Write the JSON line
+            file.write(json.dumps({
+                "page_id": page_id,
+                "title": page_title.replace('_', ' '),
+                "is_redirect": page_is_redirect,
+                "namespace": page_namespace,
+                "languages": languages
+            }, ensure_ascii=False) + '\n')
+        except Exception as e:
+            errored_words.append(page_title)
+
+# Save errored words to a file
+with open('errored_words.txt', 'w', encoding="utf-8") as file:
+    for word in errored_words:
+        file.write(word + '\n')
 
 cursor.close()
 conn.close()
